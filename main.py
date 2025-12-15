1b96afb74094463c15a84298cec03fd12f4581e5
jpvargues <j.p.b.vargues@gmail.com>
2025-12-15 18:17:26 +0000

    Added get_book_text and count_words on main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        return file.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_text = get_book_text('books/frankenstein.txt')
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
+
+if __name__ == "__main__":
+    main()
